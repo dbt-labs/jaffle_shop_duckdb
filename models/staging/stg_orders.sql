4166d6c9338706788623f536d185afe7182d83db
SoumayaMauthoorMOJ <soumaya.mauthoor@digital.justice.gov.uk>
2023-09-17 22:13:11 +0100

    restructure and add group

--- models/staging/stg_orders.sql
+++
@@ -1,9 +0,0 @@
-{{ config(
-    materialized='view',
-    post_hook=[ 
-        "DROP VIEW {{ this }}",
-        "ALTER TABLE {{ ref('stg_orders_wap') }} RENAME TO {{ this.identifier }}",
-    ],
-) }}
-
-select * from {{ ref('stg_orders_wap') }}
--- models/staging/stg_payments.sql
+++
@@ -1,9 +0,0 @@
-{{ config(
-    materialized='view',
-    post_hook=[ 
-        "DROP VIEW {{ this }}",
-        "ALTER TABLE {{ ref('stg_payments_wap') }} RENAME TO {{ this.identifier }}",
-    ],
-) }}
-
-select * from {{ ref('stg_payments_wap') }}
